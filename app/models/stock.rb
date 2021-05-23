dcc06c0e0dd0ada544364e73b9f6772b5b1c1137
Batuhan Özcan <batuhan.ozcan@bil.omu.edu.tr>
2021-05-23 15:09:35 +0300

    edit my_portfolio page ui

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,5 @@
 class Stock < ApplicationRecord
 
-
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:sandbox_published_key],
@@ -8,6 +7,8 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
 
-    client.price(ticker_symbol)
+    new(ticker: ticker_symbol,
+        name: client.company(ticker_symbol).company_name,
+        last_price: client.price(ticker_symbol))
   end
 end

ee2e3209027fb264373bfa8f40492d398aa414ed
Batuhan Özcan <batuhan.ozcan@bil.omu.edu.tr>
2021-05-29 15:48:14 +0300

    add search friend

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :user_stocks, only: %i[create destroy]
   devise_for :users
   root 'home#index'
-  get 'my_portfolio', to: 'users#my_portfolio'
-  get 'search_stock', to: 'stocks#search'
-  get 'my_friends', to: 'users#my_friends'
+  get 'my_portfolio',   to: 'users#my_portfolio'
+  get 'search_stock',   to: 'stocks#search'
+  get 'my_friends',     to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
 end
